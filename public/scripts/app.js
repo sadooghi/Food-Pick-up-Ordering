8bf3af202d83652e7d7497306f024e291d6598f1
Suraj Mistry <mistrs9@mcmaster.ca>
2017-03-30 18:08:58 +0000

    Front End: Most buttons exist, page skeleton complete

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,7 +6,7 @@ $(() => {
     users = $.grep(users, function(param, index){
         return(param.description != undefined);
       });
-    var outStr = '<div class="col-sm-8 container food"><div class="row">';
+    var outStr = '<div class="container scrollers"><div class="col-sm-8 food"><div class="row">';
     var counter = 0;
     for(user of users) {
       counter ++;
@@ -17,6 +17,15 @@ $(() => {
       }
     }
     outStr+= '</div></div>';
+    //START THE CART DISPLAY
+    outStr += "<div class='col-sm-4 cart'><div class='row'><h4 id='order-head'>Add to your Cart!</h4>";
+    //LOOP THROUGH CART ATTRIBUTES LATER BUT FOR NOW JUST FOODS
+    for(user of users) {
+      outStr += createCartElement(user);
+    }
+    //INSERT THE CART DISPLAY HERE
+    outStr += "</div></div></div>";
+
     $("<div class='row'>").html(outStr).appendTo($("div"));
   });
 });
@@ -28,11 +37,24 @@ function createFoodElement (food){
       <img class="img-responsive" src="${food.picture}">
       <b>${food.name}</b>, Price: ${food.price}
       <button class="btn">+1</button>
-      <br>
       <span class="description">
       ${food.description}
       </span>
 
-     </div>`
+     </div>`;
   return $sendFood;
 }
+
+function createCartElement (food){
+  var $sendItem =
+  `<div class="col-lg-12">
+    <img class="img-responsive cart-pic" src="${food.picture}">
+    <span class="name">${food.name}</span>
+    <button class="btn incr">+1</button>
+    <button class="btn decr">-1</button>
+
+  </div>
+  `;
+  return $sendItem
+}
+
