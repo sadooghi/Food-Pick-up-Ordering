21a2e570e3e47d061dbe99044f0ac484adb2f003
sadooghi <gh.sadooghi@gmail.com>
2017-03-30 18:55:55 +0000

    added food_access.js and related files

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,24 +1,39 @@
 $(() => {
+  // $.ajax({
+  //   method: "GET",
+  //   url: "/api/users"
+  // }).done((users) => {
+  //   users = $.grep(users, function(param, index){
+  //       return(param.description != undefined);
+  //     });
+  //   var outStr = '<div class="col-sm-8 container food"><div class="row">';
+  //   var counter = 0;
+  //   for(user of users) {
+  //     counter ++;
+  //     outStr += createFoodElement(user);
+  //     if(counter > 1) {
+  //       counter = 0;
+  //       outStr += '</div><div class="row">'
+  //     }
+  //   }
+  //   outStr+= '</div></div>';
+  //   $("<div class='row'>").html(outStr).appendTo($("div"));
+  // });
+
+
+
   $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    users = $.grep(users, function(param, index){
-        return(param.description != undefined);
-      });
-    var outStr = '<div class="col-sm-8 container food"><div class="row">';
-    var counter = 0;
-    for(user of users) {
-      counter ++;
-      outStr += createFoodElement(user);
-      if(counter > 1) {
-        counter = 0;
-        outStr += '</div><div class="row">'
-      }
+    method:"GET",
+    url: "/menu/"
+  }).done((food)=>{
+
+    for(item of food) {
+      $("<div>").text(item.name).appendTo($("body"));
+
     }
-    outStr+= '</div></div>';
-    $("<div class='row'>").html(outStr).appendTo($("div"));
-  });
+
+  })
+
 });
 
 
---
+++ routes/food_access.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const express = require('express');
+const router  = express.Router();
+// const knexConfig = require('./knexfile')
+// const knex = require('knex')(knexConfig.development)
+
+module.exports = (knex) => {
+
+  router.get("/", (req, res) => {
+    knex
+      .select("*")
+      .from("foods")
+      .then((results) => {
+
+        console.log(results);
+        res.json(results);
+    });
+  });
+
+  return router;
+}
